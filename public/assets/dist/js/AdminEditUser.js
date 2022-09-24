433bfbb89cae995500846682392499c090680bff
Fer <fernandom@aesoftware.com.mx>
2022-09-24 13:36:40 -0500

    Se edit gitignore para subir js

---
+++ public/assets/dist/js/AdminEditUser.js
@@ -0,0 +1,39 @@
+
+$('.modulo').on('switchChange.bootstrapSwitch', function (event, state) {
+    var id = $(this).attr("id");
+    var name = $(this).attr("name");
+    var val = 0;
+    if($(this).is(':checked')) {
+      val = 1;
+    } else {
+      val = 0;
+    }
+    
+    document.getElementById("idEdit").value = id;
+    document.getElementById("valEdit").value = val;
+    document.getElementById("nameEdit").value = name;
+
+
+    $('#exampleModal').modal('show');
+});
+
+
+$('#closeModal').click( function(){
+
+	var valcheck = false;
+	var val = $('#valEdit').val();
+	var name = $('#nameEdit').val();
+	if (val == 0) {
+		valcheck = true;
+	} else {
+		valcheck = false;
+	}
+
+	$("[name='"+name+"']").bootstrapSwitch('state',  valcheck );
+
+	$('#frmEditPermiso')[0].reset();
+	$('#exampleModal').modal('hide');
+
+
+})
+
---
+++ public/assets/dist/js/asignaPass.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+  
+  $("#passUpdate").click(function(){
+    $("#spin").addClass( "spinner-border" );
+    var formData = new FormData($("form#data")[0]);
+  
+    $.ajax({
+      url: base_url + '/asignaPass',
+      type: 'POST',
+      dataType: 'json',
+      data: formData,
+      cache:false,
+      async:true,
+      contentType: false,
+      processData: false,
+      success:function(response){
+        $('.error').remove();
+        if (response.succes.succes == 'succes') {
+          toastr.success(response.succes.mensaje);
+
+          var count = 5;
+          setInterval(function(){
+            count--;
+            if (count == 0) {
+              window.location = base_url; 
+            }
+          },1000);
+
+        } else if (response.dontsucces.error == 'error'){
+          toastr.error(response.dontsucces.mensaje);                             
+        } else if (Object.keys(response.error).length > 0 ){
+
+            for (var clave in response.error){
+                  
+              $( "<div class='error input-group text-danger'>&nbsp;" + response.error[clave] + "</div>" ).insertAfter( '#lbl' + clave  );
+                
+            }
+            toastr.error('Todos los campos son obligatorios');
+        } 
+      $("#spin").removeClass( "spinner-border" );
+      },
+      error: function(jqXHR, textStatus, errorThrown){
+      toastr.error('Error , intente nuevamemte');
+      $("#spin").removeClass( "spinner-border" );
+      }
+    });     
+  });
+
+
+});
